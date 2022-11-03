9e6030abf9026f26d36d6b91716f4382fb72707b
ramarao121 <111497833+ramarao121@users.noreply.github.com>
2022-11-03 22:48:59 +0530

    n,m=map(int,input().split()) a=max(n,m) while True:     if n%a==0 and m%a==0:         print(a)         break     a=a-1

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,7 @@
 n,m=map(int,input().split())
-s=max(n,m)
-while "True":
-    if n%s==0 and m%s==0:
-        print(s)
-        break 
-    s=s-1
+a=max(n,m)
+while True:
+    if n%a==0 and m%a==0:
+        print(a)
+        break
+    a=a-1
